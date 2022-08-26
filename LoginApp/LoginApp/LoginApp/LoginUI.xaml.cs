9e0431299c4f745fa88a984e7f1eb98809860706
Muhammed Taha <cermuhammed720@gmail.com>
2022-08-26 15:29:22 +0300

    Initial commit

---
+++ LoginApp/LoginApp/LoginApp/LoginUI.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+             
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (sicil == null)
+            {
+                await DisplayAlert("uyarı", "lütfen sicil numaranızı giriniz", "ok");
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(sicil.Text) == true)
+                {
+                    await DisplayAlert("uyarı", "lütfen sicil numaranızı girin", "ok");
+
+                }
+                //else await navigation.pushasync(new mainpage());
+
+            }
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //sicil.Focus();
+        }
+
+        
+    }
+}
---
+++ LoginApp/LoginApp/LoginApp/Showide.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace LoginApp
+{
+    internal class Showide : TriggerAction<ImageButton>, INotifyPropertyChanged
+    {
+        public string Show { get; set; }
+        public string Hide { get; set; }
+
+        bool _hidePassword = true;
+        public bool HidePassword
+        {
+            get=> _hidePassword;
+            set
+            {
+                if (_hidePassword != value)
+                {
+                    _hidePassword = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(HidePassword)));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected override void Invoke(ImageButton sender)
+        {
+            sender.Source = HidePassword ? Show : Show;
+            HidePassword = !HidePassword;
+        }
+
+    }
+}
